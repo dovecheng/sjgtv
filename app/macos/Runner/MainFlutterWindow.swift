a7e19a00803539d4ac8e6a97d4364f3bcc3f80e5
dovecheng <chenglaian@qq.com>
2026-02-02 02:45:51 +0800

    feat: PC 端窗口 800x600、启动居中、最小尺寸 800x600

--- app/macos/Runner/MainFlutterWindow.swift
+++ app/macos/Runner/MainFlutterWindow.swift
@@ -3,11 +3,17 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
+    minSize = NSSize(width: 800, height: 600)
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
-
+    if let screen = NSScreen.main?.visibleFrame {
+      let x = screen.midX - windowFrame.width / 2
+      let y = screen.midY - windowFrame.height / 2
+      setFrame(NSRect(x: x, y: y, width: windowFrame.width, height: windowFrame.height), display: true)
+    } else {
+      setFrame(windowFrame, display: true)
+    }
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
