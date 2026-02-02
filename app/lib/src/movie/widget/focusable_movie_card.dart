162235146fe95dd40fbd04b21330f5972102e09d
daveli <daveli@mrcdevlab.com>
2026-02-02 03:21:27 +0000

    fix 豆瓣API 电影返回的封面图 加入 Referer

--- app/lib/src/movie/widget/focusable_movie_card.dart
+++ app/lib/src/movie/widget/focusable_movie_card.dart
@@ -72,6 +72,7 @@ class _FocusableMovieCardState extends State<FocusableMovieCard> {
                                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                               'Accept':
                                   'image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+                              'Referer': 'https://movie.douban.com/',
                             },
                             fit: BoxFit.cover,
                             width: double.infinity,
