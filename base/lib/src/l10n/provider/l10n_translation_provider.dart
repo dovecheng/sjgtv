e3cd9519037df7b72d25fb8205a8e0735077103b
dovecheng <chenglaian@qq.com>
2026-02-02 14:46:37 +0800

    chore: 删除未使用的 API 相关文件和搜索功能扩展

--- base/lib/src/l10n/provider/l10n_translation_provider.dart
+++ base/lib/src/l10n/provider/l10n_translation_provider.dart
@@ -40,16 +40,18 @@ class L10nTranslationProvider extends _$L10nTranslationProvider {
     if (tr == null && presetTranslations.isNotEmpty) {
       tr = presetTranslations.first;
     }
-    if (tr == null) {
-      tr = L10nTranslationModel(languageTag: 'en', translations: <String, String>{});
-    }
+    tr ??= L10nTranslationModel(
+      languageTag: 'en',
+      translations: <String, String>{},
+    );
     final L10nTranslationModel result = tr;
 
-    if (presetTranslations != L10n.translations && L10n.translations.isNotEmpty) {
+    if (presetTranslations != L10n.translations &&
+        L10n.translations.isNotEmpty) {
       L10nTranslationModel? baseTr = L10n.translations.firstWhereOrNull(
         (L10nTranslationModel e) => e.languageTag == result.languageTag,
       );
-      if (baseTr == null) baseTr = L10n.translations.first;
+      baseTr ??= L10n.translations.first;
       result.translations = {...?baseTr.translations, ...?result.translations};
     }
 
